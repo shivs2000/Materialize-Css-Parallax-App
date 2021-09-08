589dee81e0b71735e6befb9615f4910563320ba2
shivs2000 <singhshivam0666@gmail.com>
2021-09-08 19:25:42 +0530

    1st

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//sidenav
+var sidenav=document.querySelector('.sidenav');
+M.Sidenav.init(sidenav,{});
+
+//Parallax
+var Parallax=document.querySelectorAll('.parallax');
+M.Parallax.init(Parallax),{};
+
+//materialboxed
+var mb=document.querySelector('.materialboxed');
+M.Materialbox.init(mb,{});
+//
+var ss=document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(ss,{})
